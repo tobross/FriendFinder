701d2e3c5497c0462eaac9e0a47983c09c3b8198
Tobey Ross <tobross@gmail.com>
2018-05-22 16:57:23 -0500

    committing initial file structure and example friend array

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+[{
+    "name":"Tobey",
+    "photo":"tobeypic.png",
+    "scores":[
+        5,
+        4,
+        3,
+        2,
+        1,
+        2,
+        3,
+        4,
+        5,
+        4
+    ]
+}]
